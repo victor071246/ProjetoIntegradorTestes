2ec6d1d36aa60a1fb0479286155bfb3f93aeb17b
victor071246 <daderiovictorfelipe@gmail.com>
2025-04-29 13:58:54 -0300

    Server working

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 import routes from "./routes";
 import cors from "cors";
 import dotenv from "dotenv";
+
+// Carrega as variáveis de ambiente
 dotenv.config();
+
+// Verifique se a variável MONGO_URI foi carregada
+console.log("MONGO_URI:", process.env.MONGO_URI); // Isso deve imprimir o valor da variável de ambiente
 
 class App {
   constructor() {
@@ -11,7 +16,18 @@ class App {
 
     const mongoURI = process.env.MONGO_URI;
 
-    mongoose.connect(mongoURI);
+    if (!mongoURI) {
+      console.error("MONGO_URI não está definido!");
+      process.exit(1); // Encerra o processo caso a variável não esteja definida
+    }
+
+    mongoose
+      .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => console.log("Conectado ao MongoDB com sucesso!"))
+      .catch((err) => {
+        console.error("Erro ao conectar ao MongoDB:", err);
+        process.exit(1);
+      });
 
     this.middlewares();
     this.routes();
